416c0b8737c56d4d4a73aa9e90fdbd41e9312213
andreaconv <andrea2convertini@gmail.com>
2023-10-02 15:49:23 +0200

    step 1 - [add] HTML con nome passato come attributo

--- src/main/java/org/java/app/controller/MainController.java
+++ src/main/java/org/java/app/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.java.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,15 @@ public class MainController {
 	@ResponseBody
 	public String test() {
 		return "Hello world";
+	}
+	
+	@GetMapping("/step1")
+	public String test1(Model model) {
+		
+		final String name = "Andrea";
+		
+		model.addAttribute("name", name);
+		
+		return "index";
 	}
 }
